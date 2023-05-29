f3c5c73352f1b277e30e76e9bc572f277548194e
AuoozAhsan <auooz.akbar1996@gmail.com>
2023-05-29 09:17:44 -0400

    initial commit

--- src/math_problems/Factorial.java
+++ src/math_problems/Factorial.java
@@ -1,7 +1,8 @@
 package math_problems;
 
-public class Factorial {
+import javax.swing.*;
 
+public class Factorial {
     /** INSTRUCTIONS
      *
      * Write a method to return the Factorial of any given number using Recursion, and also another way, using iteration
@@ -10,9 +11,17 @@ public class Factorial {
      * HINT: Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
 
+    static int factorial(int f)
+    {
+        if ( f== 0)
+            return 1;
+
+        return f * factorial(f - 1);
+    }
+
     public static void main(String[] args) {
-
-
+        int number = 7;
+        System.out.println("Factorial of " + number + " is "+ factorial(7));
     }
 
 }

53804497ddaf1bf196dfde67e849e84d41b9dc8e
AuoozAhsan <auooz.akbar1996@gmail.com>
2023-05-30 16:02:44 -0400

    initial commit

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -6,10 +6,25 @@ public class LowestNumber {
      * Write a method to find the lowest number from this array.
      */
 
+    public static int lowestNumber(int[] array){
+        int minimumNumber = array[0];
+        for (int i = 0; i < array.length; i++){
+            if(array[i] < minimumNumber){
+                minimumNumber = array[i];
+            }
+        }
+        return minimumNumber;
+    }
+
     public static void main(String[] args) {
 
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
+
+        System.out.println("Lowest number in array is " + lowestNumber(array));
+
+
+
 
 
     }
